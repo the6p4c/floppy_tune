60a70189864eae95e7a898a2c2203a0276095b66
the6p4c <me@doggirl.gay>
2018-10-14 15:58:35 +1000

    Read first cluster of file

--- boot.asm
+++ boot.asm
@@ -21,6 +21,13 @@ _start:
 	mov ax, 0x7C0
 	mov ds, ax
 
+	; Read/calculate some necessary constants
+	mov al, byte [ds:13]
+	mov byte [fat_spc], al
+
+	; TODO: Actually calculate this based on the BPB
+	mov word [fat_ssa], (0x0001 + 0x02 * 0x0009 + 32 * 0x00E0 / 0x0200)
+
 	; Find the first root directory sector
 	; = table_count * fat_size + reserved_sector_count
 	xor ax, ax
@@ -37,7 +44,7 @@ _start:
 	mov es, bx
 	xor bx, bx
 
-	xor ch, ch ; cylinder 0
+	; cylinder already set
 	mov dh, al ; head number
 	mov cl, ah ; sector number
 	mov al, 1 ; read 1 sector
@@ -46,8 +53,6 @@ _start:
 	mov ah, 0x02
 	int 0x13
 
-	xor dx, dx ; cursor position
-
 	; Find the first file on the drive
 .parse_entry:
 	mov al, byte [es:bx]
@@ -55,47 +60,83 @@ _start:
 	je .parse_finished ; no more entries in this directory
 
 	cmp al, 0xE5
-	je .next_entry ; entry was unused
+	jne .parse_finished ; entry is a valid file 
 
-	; Print the file name
-	push bx ; save the current entry pointer
-
-	mov bp, bx ; string address (es:bp - es already set)
-	mov al, 1 ; write mode 1 (update cursor, no attrs)
-	xor bh, bh ; page number 0
-	mov bl, 0x0F ; attribute
-	mov cx, (8+3) ; number of characters
-
-	mov ah, 0x13
-	int 0x10
-
-	inc dh ; increment cursor row
-
-	pop bx ; restore entry pointer
-	
-.next_entry:
 	add bx, 32 ; move to next entry
 	jmp .parse_entry
 
 .parse_finished:
+	; Being here means we either found a file, or there were no files
+	; Let's assume we found a file for now
+
+	; Get the file's first cluster number
+	add bx, 26 ; no issue mangling the entry pointer
+	mov ax, word [es:bx]
+
+	; Convert CN to CHS
+	call cn_to_lsn
+	call lsn_to_chs
+
+	; Read the first cluster into RAM
+	; Read to 0x120:0 = 0x1200
+	mov bx, 0x120
+	mov es, bx
+	xor bx, bx
+
+	; cylinder already set
+	mov dh, al ; head number
+	mov cl, ah ; sector number
+	mov al, 1 ; read 1 sector
+	mov dl, byte [drive_number] ; read from boot drive
+
+	mov ah, 0x02
+	int 0x13
 
 .halt:
 	hlt
 	jmp .halt
 
-; Converts a LSN to a CHS address.
+; Converts a LSN (logical sector number) to a CHS (cylinder head sector)
+; address.
 ;
 ; Inputs:
 ; 	ax: LSN
 ; Outputs:
 ; 	al: Head number
 ; 	ah: Sector number
+;	ch: Cylinder number
+; Clobbers:
+;	cl
 lsn_to_chs:
-	push cx
 	mov cl, 18 ; number of sectors per track
-	div cl ; al = head number, ah = sector - 1
+	div cl ; al = head number + cyl number * heads/cyl, ah = sector - 1
 	inc ah ; ah = sector
-	pop cx
+
+	mov ch, ah
+	xor ah, ah
+	mov cl, 2 ; number of heads per cylinder
+	div cl ; al = cyl number, ah = head number
+
+	mov cl, ch
+	mov ch, al
+	mov al, ah
+	mov ah, cl
+
+	ret
+
+; Converts a CN (cluster number) to a LSN (logical sector number).
+; Requires fat_ssa and fat_spc to be pre-calculated.
+;
+; LSN = (CN - 2) * fat_spc + fat_ssa
+;
+; Inputs:
+; 	ax: CN
+; Outputs:
+; 	ax: LSN
+cn_to_lsn:
+	sub ax, 2
+	mul word [fat_spc]
+	add ax, word [fat_ssa]
 	ret
 
 ; Ensure we're bootable - mkfs.vfat already includes a boot signature, but we'll
@@ -104,5 +145,11 @@ TIMES 510 - ($ - $$) db 0x00
 db 0x55
 db 0xAA
 
+; TODO: Fix the segmentation issues here - I have no clue where these variables
+; are actually being stored
 drive_number:
+	resb 1
+fat_ssa:
+	resw 1
+fat_spc:
 	resb 1
